f96a3d9068700dc61c8cfcbbd2a90569cb09ee3d
KnightRunner99 <98553804+KnightRunner99@users.noreply.github.com>
2022-05-13 12:22:48 -0400

    Delete hawks_fashion directory

--- hawks_fashion/Logout.php
+++
@@ -1,3 +0,0 @@
-<form class="form-inline my-2 my-lg-0">
-        <a href="Logout2.php" data-toggle="tooltip" class="btn btn-danger" ><i ></i>Logout</a>
-</form>
--- hawks_fashion/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include 'Functions/ezdubsdatabase.php';
-include 'Functions/customer_db.php';
-$id = $_SESSION['find_user'];
-delete_customer($id);
-header('location: Edit_Accounts.php');
- ?>
